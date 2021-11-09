4f6abf3c422488472db7c66c970af09761fb37c9
David Luca <David.Luca@endava.com>
2021-11-09 10:37:33 +0200

    Finish endpoint calculate missing column values

--- calc_app/models.py
+++ calc_app/models.py
@@ -1,7 +1,24 @@
 from django.db import models
 
 
+def calculate_missing_col(s, v, t):
+    if s is None:
+        s = v * t
+    elif v is None:
+        v = s / t
+    else:
+        t = s / v
+
+    return s, v, t
+
+
 class RequestModel(models.Model):
+    file_name = models.CharField(max_length=100)
+
+
+class RowModel(models.Model):
+    request_id = models.ForeignKey(RequestModel, on_delete=models.CASCADE, related_name='rows')
+
     s = models.CharField(max_length=50)
     v = models.CharField(max_length=50)
     t = models.CharField(max_length=50)
--- calc_app/views.py
+++ calc_app/views.py
@@ -3,26 +3,29 @@ import pandas as pd
 
 from django.http import HttpResponse
 
-from calc_app.models import RequestModel
+from calc_app.models import RequestModel, RowModel, ResponseModel, calculate_missing_col
 
 
 def calculate(request):
-    # if request.method == 'POST':
-    csv_file = request.body.decode()
+    decoded_body = request.body.decode()
+    file_name = decoded_body[decoded_body.find('filename=')+10: decoded_body.find('.csv')+4]
+    req = RequestModel(file_name=file_name)
+    req.save()
 
-    col_list = ["S", "V", "T"]
-    df = pd.read_csv(csv_file, usecols=col_list)
+    header = ['S', 'V', 'T']
 
-    # reader = csv.reader(csv_file)
-    # for row in reader:
-    #     print(row)
-        # _, created = RequestModel.objects.get_or_create(
-        #     first_name=row[0],
-        #     last_name=row[1],
-        #     middle_name=row[2],
-        # )
+    cr = csv.reader(decoded_body.splitlines(), delimiter=',')
+    my_list = list(cr)
+    flag = False
+    for row in my_list:
+        if flag is True and row:
+            s, v, t = calculate_missing_col(row[0], row[1], row[2])
+            row = RowModel(s=s, v=v, t=t, request_id=req.id)
+            row.save()
+        if row == header:
+            flag = True
 
-    # return HttpResponse(response)
+    return HttpResponse('all good', status=200)
 
 
 def responses(request, question_id):
