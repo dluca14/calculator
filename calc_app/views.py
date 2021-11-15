ca07ddfb2086c183f8d01f67d0a42396f043cfab
David Luca <David.Luca@endava.com>
2021-11-15 13:58:16 +0200

    Import from pypi package

--- calc_app/views.py
+++ calc_app/views.py
@@ -1,17 +1,18 @@
 import csv
 import json
 from datetime import timedelta
+from business_logic.timer import Timer
+from business_logic.process_csv_file import get_file_name, get_file_content
 
 from django.http import HttpResponse
 
 from calc_app.models import RequestModel, RowModel, ResponseModel
-from business_logic.timer import Timer
-from business_logic.utils import get_file_name, process_file
 
 
 def calculate(request):
     with Timer('calculate_view') as timer:
         decoded_body = request.body.decode()
+
         file_name = get_file_name(decoded_body)
         req = RequestModel.objects.create(file_name=file_name)
 
@@ -22,7 +23,7 @@ def calculate(request):
 
         writer = csv.writer(response)
         writer.writerow(['S', 'V', 'T'])
-        file_content = process_file(decoded_body)
+        file_content = get_file_content(decoded_body)
         for row in file_content:
             writer.writerow(row.values())
             RowModel.objects.create(s=row['s'], v=row['v'], t=row['t'], request=req)
